50560471396000e6ad037c9f3c45f249f9a963a7
Lithiumsss <empresarcap2025@gmail.com>
2026-09-11 14:01:28 -0500

    Refactor LITHIUMS1.py to improve functionality

--- LITHIUMS1.py
+++ LITHIUMS1.py
@@ -140,7 +140,7 @@ def run():
     dr = iniciar()
     pid = dr.service.process.pid
     total_disparos_global = 0
-    MAX_DISPAROS_CICLO = 1000
+    MAX_DISPAROS_CICLO = 50000
     
     try:
         handles = abrir_y_verificar_tags_todos(dr)
